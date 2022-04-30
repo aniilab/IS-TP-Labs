c70183657b65025640ad00ab93fd57e35d8e4bab
Alina <91917355+aniilab@users.noreply.github.com>
2022-04-30 23:39:00 +0300

    1.7 added

---
+++ Lab1. MovieWebApplication/MoviesWebApplication/Models/Playlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoviesWebApplication
+{
+    public partial class Playlist
+    {
+        public Playlist()
+        {
+            MoviesInPlaylists = new HashSet<MoviesInPlaylist>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+
+        public virtual ICollection<MoviesInPlaylist> MoviesInPlaylists { get; set; }
+    }
+}
